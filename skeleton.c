832c17cc72d0c92cd1fa7ac11613c46c0170d85d
cscho-csrc <161668771+cscho-csrc@users.noreply.github.com>
2024-04-05 12:36:04 -0700

    Update skeleton.c
    Went back to earlier version of code with the findMeanVector modifies *meanVector in place instead of outputting pointer. Realized the E step doesn't require calculating mean vector for each component. The "mean vector" in the text is the center, which is initialized and then updated in the M step.
    
    Wrote the E step and parallelized row by row using omp parallel for. Needed to write support functions find_determinant, gaussJordan (for the inverse) and pdf.

--- skeleton.c
+++ skeleton.c
@@ -7,15 +7,18 @@
 
 // reference https://ieeexplore.ieee.org/document/6787289
 // data, command line
-double* findMeanVector(int num_vectors, int d, double vector_matrix[num_vectors][d]);
-
-void initializeMeans(int N, int d, int K, double X[N][d], double mu[K][d]);
-void initializeCovariances(int N, int d, double X[N][d], int K, double (*sigma)[d][d]);
+void initializeMeans(int N, int d, int K, double X[N][d], double *meanVector,double mu[K][d]);
+void findMeanVector(int N, int d, double X[N][d], double *meanVector);
+void initializeCovariances(int N, int d, double X[N][d], int K, double *meanVector, double (*sigma)[d][d]);
 void initializeCoefficients(int K, double *alpha);
 
-void EStep(int N, int d, int K, double X[N][d], double mu[K][d], double sigma[d][d], double alpha, double H);
-void MStep0(int N, int d, int K, double X[N][d], double H, double mu[K][d], double alpha);
-void MStep1(int N, int d, int K, double X[N][d], double H, double mu[K][d], double alpha, double sigma[d][d]);
+double find_determinant(int n, double sigma_m[n][n]);
+void gaussJordan(int n, double matrix[n][n], double inverse[n][n]);
+double pdf(int d, double sigma_m[d][d], double mu_m[d], double x_i[d]);
+
+void EStep(int d, int K, double x_i[d], double mu[K][d], double (*sigma)[d][d], double alpha[K], double H_i[K]);
+void MStep0(int N, int d, int K, double X[N][d], double H, double mu[K][d], double *alpha);
+void MStep1(int N, int d, int K, double X[N][d], double H, double mu[K][d], double *alpha, double (*sigma)[d][d]);
 void checkConvergence(int N, int K, double H, double HPrev);
 void getLabels(int N, int K, double H, int labelIndices); // labelIndices 1xN
 // H is posterior probabuility, the output
@@ -75,6 +78,7 @@ int main(int argc, char *argv[])
     double(*sigma)[d][d] = malloc(K * sizeof(double[d][d]));   // K number of matrices that are of size dxd
 
     double *alpha = malloc(K * sizeof(double));
+    double *meanVector = malloc(d * sizeof(double));
     double *labelIndices = malloc(N * sizeof(double));
 
     // https://stackoverflow.com/questions/61078280/how-to-read-a-csv-file-in-c
@@ -109,9 +113,9 @@ int main(int argc, char *argv[])
     // use whole dataset covariance to start (could add regularization matrix *number components)
     // use uniform mixing coefficients as 1/# cluster
 
+    findMeanVector(N, d, X, meanVector);  // modifies the meanVector array in place. Input to initializeMeans and initializeCovariances.
 
-
-    initializeMeans(N, d, K, X, mu);
+    initializeMeans(N, d, K, X, meanVector, mu);
     // print the values of the initial means
     printf("initial means: \n");
     for (int i = 0; i < K; i++)
@@ -130,7 +134,7 @@ int main(int argc, char *argv[])
     //     printf("%lf\n", alpha[i]);
     // }
 
-    initializeCovariances(N, d, X, K, sigma);
+    initializeCovariances(N, d, X, K, meanVector, sigma);
     // print debugging
     for (int k = 0; k < K; k++){
         printf("covariance matrix %d: \n", k);
@@ -143,21 +147,20 @@ int main(int argc, char *argv[])
         printf("\n");
     }
 
-    // 2. E-Step
+    //////// 2. E-Step ////////
 
-    /*omp_set_num_threads(thread_count);
-    # pragma omp parallel
+    omp_set_num_threads(thread_count);
+    # pragma omp parallel for
+    for (int row = 0; row < N; row++){
+        // compute values for each row
+        EStep(d, K, X[row], mu, sigma, alpha, H[row]);
+    }
+    // print debugging
+    printf("E-Step: \n");
+    printMatrix(N, K, H);
 
-    void EStep();
-    //  a. split threads over array
 
-    //  b. calculate expectations
-
-    //  c. combine likelihood data
-
-
-
-    // 3. M-Step
+    /* 3. M-Step
     void MStep0();
     //  a. create temp vectors
 
@@ -191,11 +194,11 @@ int main(int argc, char *argv[])
     free(H);
     free(HPrev);
     free(labelIndices);
+    free(meanVector);
 
     return 0;
 }
 
-// prints solution vector
 void printMatrix(int n, int m, double x[n][m])
 {
     for (int i = 0; i < n; i++)
@@ -208,33 +211,25 @@ void printMatrix(int n, int m, double x[n][m])
     }
 }
 
-// find the mean vector for all the inputs by arithmetic mean for each dimension. Outputs a 1xd array (pointer to it)
-double* findMeanVector(int num_vectors, int d, double vector_matrix[num_vectors][d])
+// find the mean vector for all the inputs by arithmetic mean for each dimension. Used by initializeMeans and initializeCovariances.
+void findMeanVector(int N, int d, double X[N][d], double *meanVector)
 {
-    // allocate memory for the meanVector array of size 1xd that will be returned
-    double *meanVector = (double*)calloc(d,sizeof(double));
-
-    for (int dim = 0; dim < d; dim++)
+    for (int i = 0; i < d; i++)
     {
         double sum = 0;
-        for (int i = 0; i < num_vectors; i++)
+        for (int j = 0; j < N; j++)
         {
-            sum += vector_matrix[i][dim];
+            sum += X[j][i];
         }
-        meanVector[dim] = sum / num_vectors;
+        meanVector[i] = sum / N;
         // print debugging
         // printf("%i: %lf \n", i, meanVector[i]);
     }
-
-    return meanVector;  // return pointer to the meanVector array
 }
 
 // initialize means by small perturbations about the mean vector
-void initializeMeans(int N, int d, int K, double X[N][d], double mu[K][d])
+void initializeMeans(int N, int d, int K, double X[N][d], double *meanVector,double mu[K][d])
 {
-    // calculate the meanVector of the full dataset
-    double *meanVector = findMeanVector(N, d, X);
-
     // add small perturbations about the mean vector
     srand(time(NULL));
     double pert_scale = 0.1 * meanVector[0]; // set the scale of the perturbations to be 10% of the first element of the mean vector
@@ -248,9 +243,6 @@ void initializeMeans(int N, int d, int K, double X[N][d], double mu[K][d])
             // printf("%i: %lf \n", i, mu[i][j]);
         }
     }
-
-    // free memory for meanVector
-    free(meanVector);
 }
 
 void initializeCoefficients(int K, double *alpha)
@@ -263,11 +255,8 @@ void initializeCoefficients(int K, double *alpha)
     }
 }
 
-void initializeCovariances(int N, int d, double X[N][d], int K, double (*sigma)[d][d])
+void initializeCovariances(int N, int d, double X[N][d], int K, double *meanVector, double (*sigma)[d][d])
 {
-    // calculate the meanVector of the full dataset
-    double *meanVector = findMeanVector(N, d, X);
-
     // initialize the covariance matrix
     double cov_matrix[d][d];
     // fill in the diagonals with the variances
@@ -306,7 +295,124 @@ void initializeCovariances(int N, int d, double X[N][d], int K, double (*sigma)[
         }
     }
 
-    // free memory for meanVector
-    free(meanVector);
+}
+
+// I need some sub-functions to call for the PDF function, which is itself needed for the E-step.
+// Includes: determinant of a matrix, inverse of a matrix
+
+// function to calculate determinant of a matrix of size d x d
+// modified from https://stackoverflow.com/questions/41384020/c-program-to-calculate-the-determinant-of-a-nxn-matrix
+double find_determinant(int n, double sigma_m[n][n]) {
+    int i, j, k, factor = 1;
+    double det = 0;
+    double newm[n - 1][n - 1];
+
+    // Base case: when matrix is a single element
+    if (n == 1) return sigma_m[0][0];
+
+    // Calculate determinant using recursion
+    for (i = 0; i < n; i++) {
+        // Create the new matrix
+        for (j = 1; j < n; j++) {
+            for (k = 0; k < n; k++) {
+                if (k != i) {
+                    newm[j - 1][k < i ? k : (k - 1)] = sigma_m[j][k];
+                }
+            }
+        }
+        det += factor * sigma_m[0][i] * find_determinant(n - 1, newm);
+        factor *= -1;
+    }
+
+    return det;
+}
+
+void gaussJordan(int n, double matrix[n][n], double inverse[n][n]) {
+    double temp;
+    double identity[n][n];
+
+    // Initialize identity matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j)
+                identity[i][j] = 1.0;
+            else
+                identity[i][j] = 0.0;
+        }
+    }
+
+    // Applying Gauss-Jordan elimination
+    for (int i = 0; i < n; i++) {
+        temp = matrix[i][i];
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] /= temp;
+            identity[i][j] /= temp;
+        }
+        for (int k = 0; k < n; k++) {
+            if (k != i) {
+                temp = matrix[k][i];
+                for (int j = 0; j < n; j++) {
+                    matrix[k][j] -= matrix[i][j] * temp;
+                    identity[k][j] -= identity[i][j] * temp;
+                }
+            }
+        }
+    }
+
+    // Copy the result to the inverse matrix 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            inverse[i][j] = identity[i][j];
+        }
+    }
+}
+
+
+
+double pdf(int d, double sigma_m[d][d], double mu_m[d], double x_i[d]) {
+    double det = find_determinant(d, sigma_m);
+    double part_1 = 1 / ( pow(2 * M_PI, d / 2.0) * sqrt(det));
+
+    // (x_i - mu_m)
+    double diff[d];
+    for (int i = 0; i < d; i++) {
+        diff[i] = x_i[i] - mu_m[i];
+    }
+
+    // initialize the inverse matrix, then fill the inverse matrix with the gaussJordan function to get the inverse of sigma_m
+    double inverse_sigma[d][d];
+    gaussJordan(d, sigma_m, inverse_sigma);
+
+    // matrix vector multiplication. initialize result vector
+    double vec[d];
+    for (int row = 0; row < d; row++) {
+        vec[row] = 0.0;
+        for (int col = 0; col < d; col++) {
+            vec[row] += inverse_sigma[row][col] * diff[col];
+        }
+    }
+
+    // vector transpose and matrix multiplication. initialize the result value
+    double val = 0.0;
+    for (int i = 0; i < d; i++) {
+        val += vec[i] * diff[i];
+    }
+
+    double part_2 = -0.5 * val;
+
+    return part_1 * exp(part_2);
+}
+
+// input is a row vector (1xd), output is a row of component probabilities (1xK) for that row vector
+void EStep(int d, int K, double x_i[d], double mu[K][d], double (*sigma)[d][d], double alpha[K], double H_i[K]){
+    for (int m = 0; m < K; m++) {
+        double numerator = alpha[m] * pdf(d, sigma[m], mu[m], x_i);
+        double denominator = 0.0;
+        for (int k = 0; k < K; k++) {
+            denominator += alpha[k] * pdf(d, sigma[k], mu[k], x_i);
+        }
+        H_i[m] = numerator / denominator;
+
+    }
 
 }
